6112d9327f9b0f6cc303f1047d1da7f073b7ba7b
TS93 <tschram93@yahoo.com>
2021-11-05 15:16:49 -0400

    copied & reassigned

--- script.js
+++ script.js
@@ -17,7 +17,7 @@
     // and we want to make a copy of it.
     const team = players;
 
-    console.log(players, team);
+    // console.log(players, team);
     // You might think we can just do something like this:
     team[2] = 'Chris';
     // however what happens when we update that array?
@@ -37,10 +37,10 @@
     // or use the new ES6 Spread
     const team4 = [...players];
     team4[3] = 'RAWR';
-    console.log(team4);
+    // console.log(team4);
 
     const team5 = Array.from(players);
-    console.log(team5);
+    // console.log(team5);
     // now when we update it, the original one isn't changed
 
     // The same thing goes for objects, let's say we have a person object
@@ -52,9 +52,30 @@
     };
 
     // and think we make a copy:
-
+    const captain = person;
+    captain.number = 67;
+    console.log(captain);
     // how do we take a copy instead?
-
+    const cap2 = Object.assign({}, person, {
+        number: 67,
+        age: 23
+    });
+    console.log(cap2);
     // We will hopefully soon see the object ...spread
-
+    const cap3 = {
+        ...person
+    };
+    console.log(cap3);
     // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+    const wes = {
+        name: 'Wes',
+        age: 100,
+        social: {
+            twitter: '@wesbos',
+            facebook: 'wesbos.developer'
+        }
+    }
+    console.clear();
+    console.log(wes);
+
+    const dev = Object.assign({}, wes);
